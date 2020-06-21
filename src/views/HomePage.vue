0cff2f5885176c387e64dd42604313e7120bf20c
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-06-21 14:18:55 +0100

    redesign homepage 🚀

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,8 +4,8 @@
     <div class="users-container mt-3">
       <figure class="user" v-for="(user) in users" :key="user.id">
         <router-link
-        tag="div"
-        class="router-link"
+          tag="div"
+          class="router-link"
           :to="{
           name: 'UserDetails',
           params: { id: user.id}
@@ -43,15 +43,10 @@ export default {
 <style>
 .users-container {
   cursor: pointer;
-  display: flex;
-  flex-wrap: wrap;
 }
 .user {
   height: 20rem;
-  margin-left: 5px;
-  margin-right: 5px;
   padding: 0.8rem;
-  flex: 1 1 200px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   background-color: #fff;
@@ -59,8 +54,30 @@ export default {
 .user:hover {
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 }
+</style>
+
+<style  scoped>
+.user {
+  border-radius: 10px 100px / 120px;
+  width: 50vw;
+  margin: auto;
+  margin-bottom: 1.6rem;
+}
 .router-link {
+  display: flex;
   width: 100%;
   height: 100%;
+  align-items: center;
+  justify-content: center;
+}
+h2 {
+  text-align: center;
+}
+@media screen  and (max-width: 768px){
+  .user {
+  width: 85vw;
+  border-radius: 10px 50px / 170px;
+
+}
 }
 </style>
